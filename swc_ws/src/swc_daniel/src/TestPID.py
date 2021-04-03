ae4ff8ed356d40626a40cc3846786e2a22e450ad
danielbrownmsm <daniel.brown@msmrockets.org>
2021-04-03 07:28:24 -0500

    made testPID better. we should use it to plot and tune some stuff

--- swc_ws/src/swc_daniel/src/TestPID.py
+++ swc_ws/src/swc_daniel/src/TestPID.py
@@ -3,44 +3,49 @@ import random
 import time
 from Util import PIDController, clamp, sign
 
-controller = PIDController(0.7, 0.00001, 0.001, velocity_threshold=0.5, threshold=0.5)
+controller = PIDController(1, 0.0001, 0.0005, velocity_threshold=0.5, threshold=0.5)
 controller.setSetpoint(100)
+random.seed(314159)
 
 pos = 0
 vel = 0
-friction = 0.5
+friction = 0.1
 
-errors = []
+positions = []
 times = []
+outputs = []
+vels = []
 iteration = 0
 
 controller.calculate(0)
 
-#while not controller.atSetpoint() and iteration < 700:
-while iteration < 300:
-    output = controller.calculate(pos + random.normalvariate(0, 1)*vel) # randomizes input (and thus output) proportional to speed
-    output = clamp(output, -2, 2)
-    #if abs(output) < 0.3 and random.random() > 0.2: # randomly if output is very small
-    #    output /= 100 # it will "be ignored" (simulating minimum output required to do things like overcoming friction)
-    vel += output
-    vel += vel * friction * -sign(vel) # - random.random()/100 # adds randomness to friction (very little)
-    vel = clamp(vel, -5, 5)
-    pos += vel
+while iteration < 1000:
+    input_ = pos + (random.normalvariate(0, 2)*vel) # add noise to input
+    
+    output = clamp(controller.calculate(input_), -7, 7) # get output and clamp it
+    output += random.normalvariate(0, 0.1) # randomise output so dead-reckoning doesn't work
 
+    vel += output / 17 # v += F/m
+    vel += vel * friction * -sign(vel) # adds friction opposing velocity
+    vel = clamp(vel, -20, 20) # limit our velocity (simulating max velocity on something)
+
+    pos += vel # update position based on velocity
+
+    # add all our data
     times.append(iteration)
-    errors.append(pos)
-    iteration += 1
-    time.sleep(0.0002)
+    positions.append(pos)
+    outputs.append(output)
+    vels.append(vel)
 
-print(controller.atSetpoint())
-print(controller.zero_divisions)
-print(controller.velocity_error)
+    iteration += 1 # step one up
+    time.sleep(0.00002) # sleep so our time.times() work
 
-setpoint_list = []
-for i in range(len(times)):
-    setpoint_list.append(controller.setpoint)
+setpoint_list = [controller.setpoint for i in range(len(times))] # to plot a straight line at setpoint
 
 fig, ax = plt.subplots()
-plt.plot(times, errors)
-plt.plot(times, setpoint_list)
+plt.plot(positions, color="blue", label="position")
+plt.plot(outputs, color="red", label="output")
+#plt.plot(vels, color="yellow", label="velocity")
+plt.plot(setpoint_list, color="green", label="setpoint")
+plt.legend()
 plt.show()
