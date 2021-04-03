ca43b854516167b6ed4a0a299e2f8933bf8aa66b
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2021-04-03 16:20:13 -0500

    tried to make a way to save PID results to look at and think about tuning later, pickle sucks

--- swc_ws/src/swc_daniel/src/ControlNode.py
+++ swc_ws/src/swc_daniel/src/ControlNode.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function, division
 from math import degrees, atan
+import pickle
 
 import rospy
 from Util import PIDController, dist, latLonToXY, clamp
@@ -11,10 +12,17 @@ from swc_msgs.srv import Waypoints
 class ControlHandler(object):
     """A class to handle controlling our robot"""
     def __init__(self, points):
-        self.distancePID = PIDController(4, 0, 0.5) #TODO tune
+        self.distancePID = PIDController(3, 0.001, 0.001) #TODO tune
         self.distancePID.setSetpoint(0)
-        self.distancePID.threshold = 0.5
-        self.distancePID.velocityThreshold = 0.5
+        self.distancePID.threshold = 2
+        self.distancePID.velocityThreshold = 4
+        self.distance_errors = {
+            0:[],
+            1:[],
+            2:[],
+            3:[],
+            4:[]
+        }
 
         self.anglePID = PIDController(0.05, 0, 0.001) #TODO tune
         self.anglePID.setSetpoint(0)
@@ -27,6 +35,7 @@ class ControlHandler(object):
         self.goal = self.points[0]
         self.target_angle = 0
         self.hasRun = False
+        self.hasDumped = False
 
     def stateCallback(self, data):
         """A callback for data published to /daniel/state about the robot's state"""
@@ -43,7 +52,16 @@ class ControlHandler(object):
         if self.distancePID.atSetpoint() and self.hasRun:
             self.targetIndex += 1
             rospy.logwarn("Going for next waypoint")
+        if self.targetIndex == 2 and not self.hasDumped:
+            print("dumping...")
+            #with open("errors.txt", "w") as f:
+            #    f.write("yeah this exists")
+            #    pickle.dump(self.distance_errors, f)
+            print(self.distance_errors)
+            print("dumped!")
+            self.hasDumped = True
         self.goal = self.points[self.targetIndex]
+        self.distance_errors[self.targetIndex].append(dist(self.state.x, self.state.y, *self.goal))
         #self.distancePID.setSetpoint(dist(self.points[self.targetIndex-1], self.points[self.targetIndex-1], *self.goal))
         #print(self.goal)
         self.target_angle = -degrees(atan((self.state.x - self.goal[0])  / (self.state.y - self.goal[1])))
@@ -95,5 +113,7 @@ if __name__ == "__main__":
         rospy.spin()
 
     except rospy.ROSInterruptException:
+        rospy.logwarn("interrupted!")
+        print("I SAID INTERRUPTED!!!")
         pass
 
